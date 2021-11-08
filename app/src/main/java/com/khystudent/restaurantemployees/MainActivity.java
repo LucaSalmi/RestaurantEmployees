25cc86098b6fe9705558ffc8c7860b7861240755
LucaSalmi <luca.salmi90@gmail.com>
2021-11-08 09:20:47 +0100

    added string

--- app/src/main/java/com/khystudent/restaurantemployees/MainActivity.java
+++ app/src/main/java/com/khystudent/restaurantemployees/MainActivity.java
@@ -85,7 +85,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
-                // TODO Auto-generated method stub
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -98,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 new DatePickerDialog(MainActivity.this, date, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
